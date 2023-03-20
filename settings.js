26191585412d405cc9f3e99ef5db9c45a0361910
Xeon54 <109366503+Xeon54@users.noreply.github.com>
2023-03-20 14:08:44 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 
 //documents variants
@@ -15,30 +15,30 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.ANTI_BADWORD = 'true' // true of false
-global.AUTO_REACT = 'false' // true of false
+global.AUTO_REACT = 'true' // true of false
 global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
-global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
-global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
-global.ytname = "YT: Dark" //ur yt chanel name
+global.alivemsg = 'XEON WHATSAPP BOT'
+global.alivepic = 'https://i.ibb.co/6N69kd5/devil-splash-logo.jpg '
+global.owner = ['923427776210'] //ur owner number
+global.ownername = "MUZAMIL XEON" //ur owner name
+global.ytname = "YT: XEON MUZAMIL" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "PAKISTAN" //ur location
 
 //bot bomdy 
 global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
-global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://www.darknero.ga/' //script link
+global.ownernomer = "923427776210" //ur number
+global.lang = "EN" //Select Language "EN" to English "SI" to sinhala 
+global.premium = ['923427776210'] //ur premium number
+global.botname = 'CHEEMS BOT ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
+global.linkz = "https://chat.whatsapp.com/HLzNkWMm53d2veD4qykp71" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/HLzNkWMm53d2veD4qykp71" //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/HLzNkWMm53d2veD4qykp71' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Dark Nero By" //ur sticker watermark packname
-global.author = "Dark Nero Creating By Cyber Yakuza Team" //ur sticker watermark author
-global.wm = "Dark Bot Inc." //ur watermark
+global.packname = "𝗫𝗘𝗢𝗡 𝗺𝘂𝘇𝗮𝗺𝗶𝗹" //ur sticker watermark packname
+global.author = "𝗖𝗥𝗘𝗔𝗧𝗘𝗗 𝗕𝗬 𝗠𝗨𝗭𝗔𝗠𝗜𝗟 𝗫𝗘𝗢𝗡" //ur sticker watermark author
+global.wm = "XEON Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
